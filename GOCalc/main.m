e257422c1dfce9ab83293763635de4c144c13925
Samuel Goodwin <goodwinlabs@gmail.com>
2011-10-04 00:24:50 -0400

    Initial Commit

---
+++ GOCalc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GOCalc
+//
+//  Created by Samuel Goodwin on 10/4/11.
+//  Copyright (c) 2011 SNAP Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
